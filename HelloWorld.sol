c7e119db1f15a57eec0417c711675c76c4161c2b
gnolankettering <129680080+gnolankettering@users.noreply.github.com>
2023-04-02 12:07:03 -0400

    Create HelloWorld.sol

---
+++ HelloWorld.sol
@@ -0,0 +1,12 @@
+// SPDX-license-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract HelloWorld
+{
+    string private stateVariable = "Hello World";
+
+    function GetHelloWorld() public view returns (string memory)
+    {
+        return stateVariable;
+    }
+}
